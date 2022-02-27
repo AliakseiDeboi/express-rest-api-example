f3e58fc4cd49ec23f041d6ac3f42a95788075d05
Aliaksei Deboi <68785467+AliakseiDeboi@users.noreply.github.com>
2022-02-27 16:07:15 +0300

    Update first.js

--- first.js
+++ first.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.get('/', function(req, res) {
   res.type('text/plain');
-  res.send('i am a beautiful butterfly');
+  res.send('i am a great person!');
 });
 
 app.listen(process.env.PORT || 4730);
